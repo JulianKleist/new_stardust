26d3c14929341d435c1f694c6a80795794db898b
JulianKleist <julianlopez.j.l.52@gmail.com>
2023-10-17 14:05:11 -0300

    testimonial card

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './blog.css';
+import './testimonials.css';
 import { AiFillStar } from "react-icons/ai";
 
 class Testimonials extends React.Component {
@@ -9,20 +9,28 @@ class Testimonials extends React.Component {
         return (
             <div className=''>
                 <div className="card border-2 mx-4" style={{ width: "100%" }}>
-                    <div className='row'>
-                        <div className='d-flex col-4'> 
-                        <AiFillStar className='bg-white text-warning' />
-                        <AiFillStar className='bg-white text-warning' />
-                        <AiFillStar className='bg-white text-warning' />
-                        <AiFillStar className='bg-white text-warning' />
-                        <AiFillStar className='bg-white text-warning' />
+                    <div className=''>
+                        <div className='px-3 pt-3'> 
+                        <AiFillStar className='text-warning' />
+                        <AiFillStar className='text-warning' />
+                        <AiFillStar className='text-warning' />
+                        <AiFillStar className='text-warning' />
+                        <AiFillStar className='text-warning' />
                         </div>
                     </div>
                     {/* <img src={"https://placehold.it/300x200"} className="card-img-top" /> */}
-                    <div className="card-body text-dark bg-white">
+                    <div className="card-body text-white">
                         {/* <h5 className="card-title bg-black">Marcas de ropa: 3 ideas de funnels</h5> */}
-                        <p className="card-text bg-white">"El equipo de TATA se conectó a nuestra compañía como un verdadero equipo de Marketing interno y se hizoz cargo de nuestro canal online de forma exitosa."</p>
-                    {/* <img src={"https://placehold.it/300x200"} className="card-img-top" /> */}
+                        <p className="card-text">"El equipo de TATA se conectó a nuestra compañía como un verdadero equipo de Marketing interno y se hizoz cargo de nuestro canal online de forma exitosa."</p>
+                        <div className='row mt-4'>
+                            <div className='d-flex justify-content-center'>
+                                <img src={"https://placehold.it/100x100"} className="profile_pic" />
+                                <div className='px-4'>
+                                <h5>F. Prado</h5>
+                                <p>Brand Manager Kazuma</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
